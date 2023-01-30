e7416b5e8e44f6eb5b7b6257216de01dd3a43166
remaciel <123792757+remaciel@users.noreply.github.com>
2023-01-29 22:25:01 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,8 +6,7 @@ document.getElementById("boton1").addEventListener("click", function()
     document.getElementById("h4s").style.color = "#ffffff"; 
     document.getElementById("footer1").style.color = "#DAD7CD";
     document.getElementById("elemento1").style.color = "#4d645c";
-    document.getElementById("elemento2").style.color = "#ffffff";
-    document.getElementById("Emergente").style.visibility = "visible";
+    document.getElementById("elemento2").style.color = "#4d645c";
     document.getElementById("grid1").style.backgroundColor = "#a3b18a";
     document.getElementById("grid2").style.backgroundColor= "#a3b18a";
     document.getElementById("grid3").style.backgroundColor = "#a3b18a";
@@ -22,7 +21,7 @@ document.getElementById("boton1").addEventListener("click", function()
 
 })
 
-document.getElementById("boton2").addEventListener("dblclick", function() 
+document.getElementById("boton2").addEventListener("click", function() 
 {   document.getElementById("boton2").style.visibility = "hidden"; 
     document.getElementById("boton1").style.visibility = "visible";
     document.getElementById("body").style.backgroundBlendMode = "normal";
@@ -35,7 +34,6 @@ document.getElementById("boton2").addEventListener("dblclick", function()
     document.getElementById("grid4").style.backgroundBlendMode = "screen";
     document.getElementById("elemento1").style.color = "#344E41";
     document.getElementById("elemento2").style.color = "#21211F";
-    document.getElementById("Emergente").style.visibility = "hidden";
     document.getElementById("footer1").style.color = "#ffffff";
     document.getElementById("parrafos").style.color = "#344E41";
 
